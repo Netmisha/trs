bfcb4ff5dbb246dd528157348d1b8a840518abc7
Mykhailo Oleksyn <mykhailo.oleksyn@synapse.com>
2016-07-01 15:06:46 +0300

    Included some more headers to Console.cpp

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -3,11 +3,15 @@
 
 #include "stdafx.h"
 #include "spdlog/spdlog.h"
-
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include <stdio.h>
 namespace spd = spdlog;
-
+void DisplayErrorBox(LPTSTR lpszFunction);
 int _tmain(int argc, _TCHAR* argv[])
 {
 	return 0;
+
 }
 
