68462b563d660d05b91417a65beae31d5481c708
Mykhailo Oleksyn <mykhailo.oleksyn@synapse.com>
2016-07-01 15:08:40 +0300

    TRSManager.cpp added

---
+++ TRSLibrary/TRSManager.cpp
@@ -0,0 +1,56 @@
+#include "TRSManager.h"
+
+TRSManager::TRSManager()
+{
+
+}
+
+TRSManager::~TRSManager()
+{
+
+}
+
+bool TRSManager::Init(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Verify(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Run(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Pause(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Stop(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::List(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Status(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Info(char* path, char* name, char* tag)
+{
+	return false;
+}
+
+bool TRSManager::Destroy(char* path, char* name, char* tag)
+{
+	return false;
+}
