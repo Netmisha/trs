7865240f87581f9503537a566213654a4140e6ea
Mykhailo Oleksyn <mykhailo.oleksyn@synapse.com>
2016-07-01 15:07:52 +0300

    dllmain.cpp updated

--- TRSLibrary/dllmain.cpp
+++ TRSLibrary/dllmain.cpp
@@ -1,5 +1,6 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#include "tinyxml.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -14,6 +15,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	case DLL_PROCESS_DETACH:
 		break;
 	}
+	
 	return TRUE;
 }
 
