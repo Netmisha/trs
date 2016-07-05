5f8e022fa44787c7070398d640d5adc48b536baf
Oleh Onyshchak <oleh.onyshchak@synapse.com>
2016-07-05 11:46:04 +0300

    I reference unittest project to TRS library, write test fo checking List function and change tests names in order to give them unique upscending names in returned list from List function

--- UnitTest/unittest1.cpp
+++ UnitTest/unittest1.cpp
@@ -1,5 +1,10 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "TRSLibrary\TRSManager.h"
+#include <list>
+
+#define AMOUNT_OF_TESTS 18
+#define MAX_TEST_NAME_LENGHT 10
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,9 +14,33 @@ namespace UnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(ListTest)
 		{
-			// TODO: Your test code here
+			Manager.Init();
+
+			std::list<Suite> list =
+				Manager.List(R"(../TestData/TestStrcuture)", nullptr, nullptr);
+
+			Assert::AreEqual(list.size(), (unsigned)AMOUNT_OF_TESTS);
+			
+			char name[MAX_TEST_NAME_LENGHT];
+			bool result = true;
+
+			std::list<Suite>::iterator iter = list.begin();
+			int i = 1;
+			for (; iter != list.end(); ++iter, ++i)
+			{
+				sprintf(name, "Test %d", i + 1);
+				int cmp_result = strcmp(name, iter->getName());
+				if (cmp_result != 0)
+				{
+					result = false;
+					break;
+				}
+			}
+			Manager.Destroy();
+
+			Assert::IsTrue(result);
 		}
 
 	};
