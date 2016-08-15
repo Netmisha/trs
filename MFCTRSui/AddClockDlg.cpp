0848d2207be54cc92d4de2874d8f0bd83dd723ce
Oleh Onyshchak <oleh.onyshchak@synapse.com>
2016-08-15 19:03:15 +0300

    Implement AddTimer dialog-box. Fix toolbar-resolution bug. Add timers icons

---
+++ MFCTRSui/AddClockDlg.cpp
@@ -0,0 +1,33 @@
+// AddClockDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "AddClockDlg.h"
+#include "afxdialogex.h"
+
+
+// AddClockDlg dialog
+
+IMPLEMENT_DYNAMIC(AddClockDlg, CDialogEx)
+
+AddClockDlg::AddClockDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(AddClockDlg::IDD, pParent)
+{
+
+}
+
+AddClockDlg::~AddClockDlg()
+{
+}
+
+void AddClockDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(AddClockDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// AddClockDlg message handlers
