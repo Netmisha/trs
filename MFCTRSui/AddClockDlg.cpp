54f308566e4708fe68d824e965429246d4b290a5
Mykhailo Oleksyn <mykhailo.oleksyn@synapse.com>
2016-08-23 14:43:20 +0300

    Tag and Name comboBoxes parsing added

--- MFCTRSui/AddClockDlg.cpp
+++ MFCTRSui/AddClockDlg.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 #include "AddClockDlg.h"
 #include "afxdialogex.h"
-
+#include "Functionality.h"
 
 // AddClockDlg dialog
 
@@ -49,7 +49,7 @@ BOOL AddClockDlg::OnInitDialog()
 		m_ListCtrl.SetCheck(i, is_check[i]);
 	}
 
-	for each (auto val in coll_name)
+	/*for each (auto val in coll_name)
 	{
 		m_EditName.AddString(val.GetString());
 	}
@@ -59,7 +59,7 @@ BOOL AddClockDlg::OnInitDialog()
 	{
 		m_EditTag.AddString(val.GetString());
 	}
-	m_EditTag.SetCurSel(tag_sel);
+	m_EditTag.SetCurSel(tag_sel);*/
 
 	CString mes;
 	CString helpMes;
@@ -204,6 +204,153 @@ void AddClockDlg::OnBnClickedOk()
 	CDialogEx::OnOK();
 }
 
+bool checkDiffName(int begin, char* source)
+{
+	int count = 0;
+	for (int i = begin; i < strlen(source); ++i)
+	{
+		if (source[i] == '\\')
+		{
+			++count;
+		}
+	}
+	return count == 1;
+}
+
+void TreeParse(std::list<Suite*>::iterator& it, std::list<Suite*>* suiteColl, int& count, std::list<Suite*>* checkList)
+{
+	// $$$ as I understood it is kind of a guard for redrawing Tree on exactly the same one?! 
+	if (count != suiteColl->size())
+	{
+		unsigned path_lenght = strlen((*it)->get_path());
+		char* rootPath = new char[path_lenght + 1];
+		strcpy_s(rootPath, path_lenght + 1, (*it)->get_path());
+
+		unsigned name_lenght = strlen((*it)->getName());
+		char* rootName = new char[name_lenght + 1];
+		strcpy_s(rootName, name_lenght + 1, (*it)->getName());
+
+		std::list<Suite*>::iterator itHelp = it;
+
+		// $$$ writing variable name in the initializing section of for-statement is useless and just slowing the program
+		for (/*$$$ it*/; it != suiteColl->end(); ++it)
+		{
+			// $$$ I suggest you to move this checking into for-statement's condition expression in the same order, as I wrote lower
+			//  count == suiteColl->size() && it != suiteColl->end()
+			if (count == suiteColl->size())
+			{
+				break;
+			}
+
+			std::list<Suite*>::iterator helper = std::find(checkList->begin(), checkList->end(), *it);
+			// @ if this suite is already in the list
+			if (helper == (checkList->end()))
+			{
+				// @ checking whether we are not iterating the root path
+				if (strlen((*it)->get_path()) != strlen(rootPath))
+				{
+					if (!strncmp(rootPath, (*it)->get_path(), strlen(rootPath)))
+					{
+						if (checkDiffName(strlen(rootPath), (*it)->get_path()))
+							TreeParse(it, suiteColl, count, checkList);
+					}
+
+				}
+				else
+				{
+					// $$$ again, despite the path checking is correct, because this else is executed only when path sizes are equal
+					// there might be bugs when comparing names using strncmp function instead of strcmp
+					// $$$ What is the purpose to compare names when pathes are identical and it is imposible for two folders to have the same path. 
+					if (!strncmp(rootPath, (*it)->get_path(), strlen(rootPath)) && !strncmp((*it)->getName(), rootName, strlen(rootName)))
+					{
+						
+						++count;
+
+						std::list<TRSTest*>::iterator iter = (*it)->getList().begin();
+						checkList->push_back((*it));
+					
+					}
+
+				}
+			}
+		}
+		--it;
+		for (/*$$$ it*/; it != suiteColl->begin(); --it)
+		{
+			// $$$ the same remark as to the loop above
+			if (count == suiteColl->size())
+			{
+				break;
+			}
+
+			std::list<Suite*>::iterator helper = std::find(checkList->begin(), checkList->end(), *it);
+			if (helper == (checkList->end()))
+			{
+				if (strlen((*it)->get_path()) != strlen(rootPath))
+				{
+					if (!strncmp(rootPath, (*it)->get_path(), strlen(rootPath)))
+					{
+						if (checkDiffName(strlen(rootPath), (*it)->get_path()))
+							TreeParse(it, suiteColl, count, checkList);
+					}
+
+				}
+				else
+				{
+					if (!strncmp(rootPath, (*it)->get_path(), strlen(rootPath)) && !strncmp((*it)->getName(), rootName, strlen(rootName)))
+					{
+						
+						++count;
+
+						std::list<TRSTest*>::iterator iter = (*it)->getList().begin();
+						
+						checkList->push_back((*it));
+						
+					}
+
+				}
+			}
+		}
+		// $$$ Eventually, I made your loop above using reverse iterator, because you yet need to process first element too)
+		// EDIT: I reconsider my descision =D
+		if (count != suiteColl->size())
+		{
+			std::list<Suite*>::iterator helper = std::find(checkList->begin(), checkList->end(), *it);
+			if (helper == checkList->end())
+			{
+
+				if (strlen((*it)->get_path()) != strlen(rootPath))
+				{
+					if (!strncmp(rootPath, (*it)->get_path(), strlen(rootPath)))
+					{
+						if (checkDiffName(strlen(rootPath), (*it)->get_path()))
+							TreeParse(it, suiteColl, count, checkList);
+					}
+				}
+				else
+				{
+
+					if (!strncmp(rootPath, (*it)->get_path(), strlen(rootPath)) && !strncmp((*it)->getName(), rootName, strlen(rootName)))
+					{
+						
+						++count;
+
+						std::list<TRSTest*>::iterator iter = (*it)->getList().begin();
+						
+						checkList->push_back((*it));
+						
+					}
+				}
+
+			}
+		}
+		it = itHelp;
+		
+		delete[] rootName;
+		delete[] rootPath;
+	}
+	
+}
 
 void AddClockDlg::OnLvnItemchangedList1(NMHDR *pNMHDR, LRESULT *pResult)
 {
@@ -218,6 +365,125 @@ void AddClockDlg::OnLvnItemchangedList1(NMHDR *pNMHDR, LRESULT *pResult)
 		{
 	//		selected_suites.push_back(SuiteRoot(m_ListCtrl.GetItemText(pNMLV->iItem, 0)));
 			selected_suites.push_back(pNMLV->iItem);
+
+			CString path = m_ListCtrl.GetItemText(pNMLV->iItem, 0);
+			char* pPa = fromCStringToChar(path);
+			std::list<Suite*>*suiteColl = Manager.List(pPa, nullptr, nullptr);
+			delete[] pPa;
+			m_EditName.Clear();
+			m_EditTag.Clear();
+			m_EditTag.ResetContent();
+			m_EditTag.AddString(L"All");
+			m_EditTag.SetCurSel(0);
+			m_EditName.ResetContent();
+			m_EditName.AddString(L"All");
+
+			m_EditName.SetCurSel(0);
+
+			m_EditThreads.SetCurSel(9);
+			int count = 0;
+			std::list<Suite*>::iterator it = suiteColl->begin();
+			count = strlen((*it)->get_path());
+			for (it; it != suiteColl->end(); ++it)
+			{
+				if (strlen((*it)->get_path()) < count)
+				{
+					count = strlen((*it)->get_path());
+				}
+			}
+			--it;
+			int lic = 0;
+			std::list<Suite*> checkList;
+			std::vector<char*> TagColl;
+			std::vector<char*> NameColl;
+			for (it; it != suiteColl->begin(); --it)
+			{
+				if (strlen((*it)->get_path()) == count)
+				{
+					TreeParse(it, suiteColl, lic, &checkList);
+				}
+			}
+			if (strlen((*it)->get_path()) == count)
+			{
+				TreeParse(it, suiteColl, lic, &checkList);
+			}
+			for (it; it != suiteColl->end(); ++it)
+			{
+				std::list<TRSTest*>::iterator iter = (*it)->getList().begin();
+
+				for (iter; iter != (*it)->getList().end(); ++iter)
+				{
+					std::vector<char*>::iterator iterator = TagColl.begin();
+					if (TagColl.size() == 0)
+					{
+						if ((*iter)->getTag())
+						{
+							std::vector<char*>::iterator first = std::find(TagColl.begin(), TagColl.end(), (*iter)->getTag());
+							if (first == TagColl.end())
+							TagColl.push_back((*iter)->getTag());
+						}
+					}
+					else
+					{
+						bool check = false;
+						for (iterator; iterator != TagColl.end(); ++iterator)
+						{
+							if (!strncmp(*iterator, (*iter)->getTag(), strlen((*iter)->getTag())))
+							{
+								check = true;
+								break;
+							}
+
+						}
+						if (!check)
+						{
+							TagColl.push_back((*iter)->getTag());
+						}
+					}
+					std::vector<char*>::iterator Nameit = NameColl.begin();
+					if (NameColl.size() == 0)
+					{
+						if ((*iter)->getName())
+						{
+							NameColl.push_back((*iter)->getName());
+						}
+					}
+					else
+					{
+						bool checkSecond = false;
+						for (Nameit; Nameit != NameColl.end(); ++Nameit)
+						{
+							if (!strncmp(*Nameit, (*iter)->getName(), strlen((*iter)->getName())))
+							{
+								checkSecond = true;
+								break;
+							}
+
+						}
+						if (!checkSecond)
+						{
+							NameColl.push_back((*iter)->getName());
+						}
+					}
+				}
+
+			}
+			for (int i = 0; i < TagColl.size(); ++i)
+			{
+				TCHAR*buf = new TCHAR[strlen(TagColl[i]) + 1];
+				convertToTCHAR(buf, TagColl[i]);
+				m_EditTag.AddString(buf);
+				delete[] buf;
+			}
+			for (int i = 0; i < NameColl.size(); ++i)
+			{
+				TCHAR*buf = new TCHAR[strlen(NameColl[i]) + 1];
+				convertToTCHAR(buf, NameColl[i]);
+				m_EditName.AddString(buf);
+				delete[] buf;
+			}
+
+
 			break;
 		}
 		case INDEXTOSTATEIMAGEMASK(BST_UNCHECKED + 1): // new state: unchecked
@@ -226,6 +492,21 @@ void AddClockDlg::OnLvnItemchangedList1(NMHDR *pNMHDR, LRESULT *pResult)
 			auto iter = std::find(selected_suites.begin(), selected_suites.end(), pNMLV->iItem);
 		    if (iter != selected_suites.end())
 				selected_suites.erase(iter);
+			m_EditTag.ResetContent();
+			m_EditTag.AddString(L"All");
+			m_EditTag.SetCurSel(0);
+			m_EditName.ResetContent();
+			m_EditName.AddString(L"All");
+
+			m_EditName.SetCurSel(0);
+			if (m_EditName.GetCurSel() > 0)
+			{
+				m_EditName.SetCurSel(-1);
+			}
+			if (m_EditTag.GetCurSel() > 0)
+			{
+				m_EditTag.SetCurSel(-1);
+			}
 			break;
 		}
 		default:
