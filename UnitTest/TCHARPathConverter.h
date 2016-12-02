0c01ff41863a83f24e7591c5d482fa2375cfef0c
Oleksandr_Mandrychenko <o.mandrychenko@globallogic.com>
2016-12-02 11:41:57 +0200

    ref

--- UnitTest/TCHARPathConverter.h
+++
@@ -1,8 +0,0 @@
-#ifndef TCHARPATHCONVERTER_H_
-#define TCHARPATHCONVERTER_H_
-#include <windows.h>
-#include <strsafe.h>
-TCHAR* convertPath(TCHAR*inPath);
-char*  convertToChar(TCHAR*);
-void  convertToTCHAR(TCHAR*, char*);
-#endif
