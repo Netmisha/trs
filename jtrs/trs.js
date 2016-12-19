84ba0b468bab79c18f4c46eb6187a17bb3fea647
Oleksandr Semeniuk <oleksandr.semeniuk@synapse.com>
2016-12-19 18:22:53 +0200

    Added libs for xml2js and fixed bugs

--- jtrs/trs.js
+++ jtrs/trs.js
@@ -1,14 +1,19 @@
 
+var suiteList = [];
 var _runScript = function(file) {
     var exec = require('child_process').exec;
-    child= exec('node '+file,(error, stdout, stderr) => {
+    child = exec('node '+file,(error, stdout, stderr) => {
         if (error) {
+            console.log("error");
             throw error;
         }
         console.log(stdout);
+        if(suiteList.length != 0) {
+            _isEnable(suiteList.shift());
+        }
     });
 }
-var _isEnable = function(dir, file) {
+var _isEnable = function(file) {
     var fs = require('fs'); 
     var xml2js = require('./xml2js/node_modules/xml2js'); 
     var parser = new xml2js.Parser();  
@@ -16,35 +21,65 @@ var _isEnable = function(dir, file) {
     parser.parseString(data, function (err, result) {
         if(result.suite.disable=="true") {
             console.log(file + " is disabled");
-            return false;
+            if(suiteList.length != 0) {
+                _isEnable(suiteList.shift());
+            }
         }
         else {
-            _runScript(dir + '/' + result.suite.test[0].execution);
-            return true;
+            var index = file.indexOf("/");
+            var jsFile;
+            while (index!=-1) {
+                var tmpIndex = file.indexOf("/", index+1);
+                if(tmpIndex==-1) {
+                    break;
+                }
+                else {
+                    index=tmpIndex;
+                }
+            }
+            index++;
+            var jsFile = file.substring(0, index) + result.suite.test[0].execution;
+            _runScript(jsFile);
         }
     });
 });
 };
-var _getAllFilesFromFolder = function(dir) {
-
-    var filesystem = require("fs");
-    var results = [];
-
-    filesystem.readdirSync(dir).forEach(function(file) {
-
+var _parseFolder = function(dir) {
+    var fileSystem = require("fs");
+    var childDir=[];
+    fileSystem.readdirSync(dir).forEach(function(file) {
         file = dir+'/'+file;
-        var stat = filesystem.statSync(file);
-
+        var stat = fileSystem.statSync(file);
         if (stat && stat.isDirectory()) {
-            results = results.concat(_getAllFilesFromFolder(file))
+            childDir.push(file);
         }
         else {
             if(file.split('.').pop()=='xml') {
-                _isEnable(dir, file);
+                suiteList.push(file);
             }
-            results.push(file);
         }
     });
-    return results;
+    childDir.forEach(function(file){
+        _parseFolder(file);
+    });
 };
-var list = _getAllFilesFromFolder('D:/Projects/trs/jtrs/Tests');
+var _runTest = function(dir) {
+    var fileSystem = require("fs");
+    var xml2js = require('./xml2js/node_modules/xml2js'); 
+    fileSystem.readdirSync(dir).forEach(function(file) {
+        file = dir+'/'+file;
+        var stat = fileSystem.statSync(file);
+        if(file.split('.').pop()=='xml') {
+            var parser = new xml2js.Parser();  
+            fileSystem.readFile(file, function(err, data) {
+                parser.parseString(data, function (err, result) {
+                    _parseFolder(dir + '/' + result.config.dir);
+                    if(suiteList.length != 0) {
+                        _isEnable(suiteList.shift());
+                    }
+                });
+            });
+        }
+    });
+};
+_runTest(__dirname);
